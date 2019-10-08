28875eb0a0e4a8d22e5b97ce6745173a23319944
huangxiaojing <huangxiaojingcn@gmail.com>
2019-10-08 14:39:13 +0800

    Feat: 构建控制，通过配置头文件 configure_file 实现对源码的控制

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "CalcPower.h"
+#include "config.h"
+
+// 通过 MY_MATH_FLAG 编译控制
+#ifdef MY_MATH_FLAG
+    #include "CalcPower.h"
+#else
+    #include <math.h>
+#endif
 
 int main(int argc, char *argv[])
 {
@@ -10,7 +17,15 @@ int main(int argc, char *argv[])
     }
     double base = atof(argv[1]);
     int exponent = atoi(argv[2]);
-    double result = power(base, exponent);
-    printf("%g ^ %d is %g\n", base, exponent, result);
+
+    #ifdef MY_MATH_FLAG
+        double result = power(base, exponent);
+        printf("使用自定义函数实现平方计算: %g ^ %d is %g\n", base, exponent, result);
+    #else
+        double result = pow(base, exponent);
+        printf("使用系统函数实现平方计算: %g ^ %d is %g\n", base, exponent, result);
+    #endif
+        printf("%g ^ %d is %g\n", base, exponent, result);
+    
     return 0;
 }
